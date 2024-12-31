ea1328dccc0831148038a3a1cb4e892027832d70
aymensellaouti <aymen.sellaouti@gmail.com>
2024-12-31 18:09:39 +0100

    fix bug

--- libs/cvs/domain/src/lib/service/embauche.service.ts
+++ libs/cvs/domain/src/lib/service/embauche.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Cv } from '../model/cv';
+import { Cv } from '../model/cv.model';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class EmbaucheService {
   private embauchees: Cv[] = [];
-
-  constructor() {}
 
   /**
    *
